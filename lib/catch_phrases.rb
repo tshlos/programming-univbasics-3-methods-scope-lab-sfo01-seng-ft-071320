810d2d27c435aae7e19312ff6fcdf765db3715bf
Taci Shlosberg <tshlosberg@gmail.com>
2020-06-11 21:58:17 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -4,7 +4,7 @@ def mario
 end
 
 
-status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+$status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 
 def toadstool
   puts status
