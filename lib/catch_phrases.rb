029eb4f53d83b8af84788bf4e067c2d5aad785df
Taci Shlosberg <tshlosberg@gmail.com>
2020-06-11 21:59:45 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,10 +1,10 @@
+$status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+
 def mario
   phrase = "It's-a me, Mario!"
   puts phrase
 end
 
-
-$status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 
 def toadstool
   puts status
