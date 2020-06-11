5d38980b028e84b915d741e9bbd60f1540e79a5b
Taci Shlosberg <tshlosberg@gmail.com>
2020-06-11 21:52:28 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,24 +1,24 @@
-# def mario
-#   phrase = "It's-a me, Mario!"
-#   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-#   puts phrase
-# end
+def mario
+  phrase = "It's-a me, Mario!"
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  puts phrase
+end
 
 
-# def toadstool
-#   puts status
-# end
+def toadstool
+  puts status
+end
 
 
-# def link 
-#   puts "It's Dangerous To Go Alone! Take This."
-# end
+def link 
+  puts "It's Dangerous To Go Alone! Take This."
+end
 
 
-# def all_phrases
-#   mario
-#   toadstool
-#   link
-# end
+def all_phrases
+  mario
+  toadstool
+  link
+end
 
-# all_phrases
+all_phrases
